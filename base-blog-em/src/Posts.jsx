51759567bb6d667be4d7deec66e0d7a23824b4ea
Moon-Sangho <skym887@gmail.com>
2022-04-27 00:16:43 +0900

    feat(base-blog-em): add isError, error, isLoading

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -16,7 +16,20 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data } = useQuery('posts', fetchPosts);
+  const { data, isError, error, isLoading } = useQuery('posts', fetchPosts);
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>Oops, something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   if (!data) {
     return <div>no data</div>;
