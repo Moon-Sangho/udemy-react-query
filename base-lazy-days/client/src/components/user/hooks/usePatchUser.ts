3c390e8fb644c114024894f898bbe39392f3e499
Moon-Sangho <skym887@gmail.com>
2022-08-30 01:38:31 +0900

    feat(base-lazy-days): add optimistic updates for user profile update

--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -1,6 +1,7 @@
 import { useCustomToast } from 'components/app/hooks/useCustomToast';
 import jsonpatch from 'fast-json-patch';
-import { UseMutateFunction, useMutation } from 'react-query';
+import { UseMutateFunction, useMutation, useQueryClient } from 'react-query';
+import { queryKeys } from 'react-query/constants';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -35,18 +36,47 @@ export function usePatchUser(): UseMutateFunction<
 > {
   const { user, updateUser } = useUser();
   const toast = useCustomToast();
+  const queryClient = useQueryClient();
 
   const { mutate: patchUser } = useMutation(
     (newUserData: User) => patchUserOnServer(newUserData, user),
     {
-      onSuccess: (userData: User | null) => {
+      // onMutate returns context that is passed to onError
+      onMutate: async (newData: User | null) => {
+        // cancel any outgoing queries for user data, so old server data
+        // doesn't overwrite our optimistic update
+        queryClient.cancelQueries(queryKeys.user);
+
+        // snapshot of previous user value
+        const previousUserData: User = queryClient.getQueryData(queryKeys.user);
+
+        // optimistically update the cache with new user value
+        updateUser(newData);
+
+        // return context object with snapshotted value
+        return { previousUserData };
+      },
+      onSuccess: () => {
         if (user) {
-          updateUser(userData);
           toast({
             title: 'User updated!',
             status: 'success',
           });
         }
+      },
+      onError: (error, newData, context) => {
+        // roll back cache to saved value
+        if (context.previousUserData) {
+          updateUser(context.previousUserData);
+          toast({
+            title: 'Update failed; restoring previous values',
+            status: 'warning',
+          });
+        }
+      },
+      onSettled: () => {
+        // invalidate user query to make sure we're in sync with server data
+        queryClient.invalidateQueries(queryKeys.user);
       },
     },
   );
--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -11,12 +11,16 @@ import {
 import type { User } from '../../../../../shared/types';
 import { useQuery, useQueryClient } from 'react-query';
 
-async function getUser(user: User | null): Promise<User | null> {
+async function getUser(
+  user: User | null,
+  signal: AbortSignal,
+): Promise<User | null> {
   if (!user) return null;
   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
     `/user/${user.id}`,
     {
       headers: getJWTHeader(user),
+      signal,
     },
   );
   return data.user;
@@ -30,11 +34,15 @@ interface UseUser {
 
 export function useUser(): UseUser {
   const queryClient = useQueryClient();
-  const { data: user } = useQuery(queryKeys.user, () => getUser(user), {
-    initialData: getStoredUser,
-    onSuccess: (received: User | null) =>
-      received ? setStoredUser(received) : clearStoredUser(),
-  });
+  const { data: user } = useQuery(
+    queryKeys.user,
+    ({ signal }) => getUser(user, signal),
+    {
+      initialData: getStoredUser,
+      onSuccess: (received: User | null) =>
+        received ? setStoredUser(received) : clearStoredUser(),
+    },
+  );
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
